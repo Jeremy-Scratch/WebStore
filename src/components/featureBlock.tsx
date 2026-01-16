2da97a5fb7c2ad11f134385f35ba3263f2030b4d
Scratch <Jeremyscratch6@gmail.com>
2026-01-16 10:44:12 -0400

    Create_new_component

---
+++ src/components/featureBlock.tsx
@@ -0,0 +1,37 @@
+import DiscountLabel from "./carousel/discountLabel";
+
+export default function FeatureBlock() {
+    return (
+        <div className="max-w-325 w-full box-border px-12.5 py-6.25 ml-auto mr-auto">
+            <div>
+                <div className="flex flex-row flex-nowrap justify-between gap-3 mb-3">
+                    <a href="">
+                        <div></div>
+                        {/* <DiscountLabel/> */}
+                    </a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                </div>
+                <div className="flex flex-row flex-nowrap justify-between gap-3 mb-3">
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                </div>
+                <div className="flex flex-row flex-nowrap justify-between gap-3 mb-3">
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                </div>
+                <div className="flex flex-row flex-nowrap justify-between gap-3 mb-3">
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
+                </div>
+            </div>
+        </div>
+    );
+}

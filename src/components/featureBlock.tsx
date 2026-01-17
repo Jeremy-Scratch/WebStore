eadbbbfee2291b3366f7bc1706cb514c1eb6d095
Scratch <Jeremyscratch6@gmail.com>
2026-01-16 20:54:42 -0400

    CLEAN - to implement Mapping

--- src/components/featureBlock.tsx
+++ src/components/featureBlock.tsx
@@ -1,6 +1,7 @@
 import DiscountLabel from "./carousel/discountLabel";
 import image from '../assets/capsule_616x353(18).jpg'
 
+
 export default function FeatureBlock() {
     const discounts = {Discount:75, BeforePrice:29.99, AfterPrice:7.49,CN:"relative"};
     return (
@@ -12,43 +13,7 @@ export default function FeatureBlock() {
                             <img src={image} alt="" className="max-w-full "/>
                         </div>
                         <DiscountLabel {...discounts} />
-                    </a>
-                    <a href="" className="relative z-1 w-[25%] block grow-0 [text-decoration:none]">
-                        <div className="hover:scale-105 relative block z-2 transition-transform duration-200 ease-in-out">
-                            <img src={image} alt="" className="max-w-full "/>
-                        </div>
-                        <DiscountLabel {...discounts} />
-                    </a>
-                    <a href="" className="relative z-1 w-[25%] block grow-0 [text-decoration:none]">
-                        <div className="hover:scale-105 relative block z-2 transition-transform duration-200 ease-in-out">
-                            <img src={image} alt="" className="max-w-full "/>
-                        </div>
-                        <DiscountLabel {...discounts} />
-                    </a>
-                    <a href="" className="relative z-1 w-[25%] block grow-0 [text-decoration:none]">
-                        <div className="hover:scale-105 relative block z-2 transition-transform duration-200 ease-in-out">
-                            <img src={image} alt="" className="max-w-full "/>
-                        </div>
-                        <DiscountLabel {...discounts} />
-                    </a>
-                </div>
-                <div className="flex flex-row flex-nowrap justify-between gap-3 mb-3">
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
-                </div>
-                <div className="flex flex-row flex-nowrap justify-between gap-3 mb-3">
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
-                </div>
-                <div className="flex flex-row flex-nowrap justify-between gap-3 mb-3">
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
-                    <a href=""></a>
+                    </a>                    
                 </div>
             </div>
         </div>

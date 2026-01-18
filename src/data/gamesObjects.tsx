38448bc9be2de4debf698a76be87ee522a33e282
Scratch <Jeremyscratch6@gmail.com>
2026-01-18 12:35:40 -0400

    NEW - gameObjects carpet: data

---
+++ src/data/gamesObjects.tsx
@@ -0,0 +1,311 @@
+import type { GameInfo } from "../components/carousel/hero/HeroCard"
+import type { DeepGameInfo } from "../components/carousel/feature/featureCard"
+
+export const Games: GameInfo[] = [
+
+    {
+        ImgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/3017860/2698868b70d1ac0595ea7e8fd9a08ea31611ca10/hero_capsule.jpg?t=1764606093",
+        BgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/3017860/ss_ba66d42e3d1ea78e5b085682484210b390eb9ccc.600x338.jpg",
+        VideoUrl: "https://video.fastly.steamstatic.com/store_trailers/3017860/1777709634/e3e986d894e749e052531d184d6de468a5d35e53/1748584907/microtrailer.webm?t=1747324264",
+        Title: "DOOM: The Dark Ages",
+        ReviewSumary: "Very Positive",
+        ReviewStat: 14262,
+        Genres: ["Action", "FPS", "Demons", "Dark Fantasy", "Singleplayer"],
+        Discount: 50,
+        BeforePrice: 69.99,
+        AfterPrice: 34.99,
+    },
+
+    {
+        ImgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1687950/hero_capsule.jpg?t=1763530018",
+        BgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1687950/ss_ddcc016b8e5c434ccbd1a89c0157ce73acf905ae.600x338.jpg",
+        VideoUrl: "https://video.fastly.steamstatic.com/store_trailers/1687950/506609/4ee1371464ce2149f69f89c36b9f906e369db2f1/1750725763/microtrailer.webm?t=1722428147",
+        Title: "Persona 5 Royal",
+        ReviewSumary: "Overwhelmingly Positive",
+        ReviewStat: 29865,
+        Genres: ["JRPG", "Story Rich", "Great Soundtrack", "Turn-Based Combat", "Anime"],
+        Discount: 70,
+        BeforePrice: 59.99,
+        AfterPrice: 17.99,
+    },
+
+    {
+        ImgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2807960/4bf05302e4ac2086fed8a75ed7cd0673602b2c62/hero_capsule.jpg?t=1765583938",
+        BgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2807960/6b43d1ae44748a7df141e9f485a5e574f8fce5bd/ss_6b43d1ae44748a7df141e9f485a5e574f8fce5bd.600x338.jpg",
+        VideoUrl: "https://video.fastly.steamstatic.com/store_trailers/2807960/1396276965/b786459af157b7e37dc4659b000ea09a8e667dc6/1763588534/microtrailer.webm?t=1763674195",
+        Title: "Battlefield™ 6",
+        ReviewSumary: "Mostly Positive",
+        ReviewStat: 123508,
+        Genres: ["FPS", "Multiplayer", "Action", "Military", "Singleplayer"],
+        Discount: 30,
+        BeforePrice: 69.99,
+        AfterPrice: 48.99,
+    },
+
+    {
+        ImgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1030300/b4bea33ed0db45aa1da0bf917abcc819faff5f2e/hero_capsule.jpg?t=1764916587",
+        BgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1030300/09ccaa6c16f158f9df8298feb5d196098506a028/ss_09ccaa6c16f158f9df8298feb5d196098506a028.600x338.jpg",
+        VideoUrl: "https://video.akamai.steamstatic.com/store_trailers/1030300/970174968/f2c514c96548048dd1dca6ef500877916b75fe5a/1755841475/microtrailer.webm?t=1764916585",
+        Title: "Hollow Knight: Silksong",
+        ReviewSumary: "Very Positive",
+        ReviewStat: 128018,
+        Genres: ["Metroidvania", "Difficult", "Indie", "Great Soundtrack", "Souls-like"],
+        Discount: 20,
+        BeforePrice: 19.99,
+        AfterPrice: 15.99,
+    },
+
+    {
+        ImgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1145350/a783c3347d40597930d282fa6727044c214e4be1/hero_capsule.jpg?t=1765831644",
+        BgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1145350/ss_b88cb7b48a86f07a7288bf37141f6558279f9bfc.600x338.jpg",
+        VideoUrl: "https://video.fastly.steamstatic.com/store_trailers/1145350/1067641820/dfba9c6a8b09f5bd9b323d6e699ac685065b93d3/1758924611/microtrailer.webm?t=1758942439",
+        Title: "Hades II",
+        ReviewSumary: "Overwhelmingly Positive",
+        ReviewStat: 57704,
+        Genres: ["Action", "Roguelike", "Roguelite", "Hack and Slash", "Mythology"],
+        Discount: 25,
+        BeforePrice: 29.99,
+        AfterPrice: 24.49,
+    },
+
+    {
+        ImgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1174180/hero_capsule.jpg?t=1759502961",
+        BgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1174180/ss_66b553f4c209476d3e4ce25fa4714002cc914c4f.600x338.jpg",
+        VideoUrl: "https://video.fastly.steamstatic.com/store_trailers/1174180/254554/90a98a4fe08d5a804167e39e6e6d6e5715ae1890/1750634082/microtrailer.webm?t=1574881947",
+        Title: "Red Dead Redemption 2",
+        ReviewSumary: "Very Positive",
+        ReviewStat: 274170,
+        Genres: ["Open World", "Story Rich", "Western", "Multiplayer", "Adventure"],
+        Discount: 75,
+        BeforePrice: 59.99,
+        AfterPrice: 14.99,
+    },
+
+    {
+        ImgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/3472040/0e9b66b404e9071551081f50ec900720f60ac4e0/hero_capsule.jpg?t=1764346316",
+        BgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/3472040/dc8acb92de608bd8e4468aee8cb64db7eed61027/ss_dc8acb92de608bd8e4468aee8cb64db7eed61027.600x338.jpg",
+        VideoUrl: "https://video.akamai.steamstatic.com/store_trailers/3472040/1059261060/b556a5ad4edc822701c0bcd2360bef335c7dbf5c/1757316395/microtrailer.webm?t=1757322769",
+        Title: "NBA 2K26",
+        ReviewSumary: "Mostly Positive",
+        ReviewStat: 3677,
+        Genres: ["Gambling", "Sports", "Basketball", "Simulation", "eSports"],
+        Discount: 60,
+        BeforePrice: 69.99,
+        AfterPrice: 27.99,
+    },
+
+    {
+        ImgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/3606480/31b4b3e440129c472e45f78ebb375b6e83f91c90/hero_capsule.jpg?t=1766101946",
+        BgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/3606480/7bce43350428d4b8859481263113e30d169451d9/ss_7bce43350428d4b8859481263113e30d169451d9.600x338.jpg",
+        VideoUrl: "https://video.fastly.steamstatic.com/store_trailers/3606480/184761750/d7a0856863e85315be3eaf959fb69cc7cb38f897/1764786481/microtrailer.webm?t=1764867756",
+        Title: "Call of Duty®: Black Ops 7",
+        ReviewSumary: "Mostly Negative",
+        ReviewStat: 1597,
+        Genres: ["Action", "FPS", "Shooter", "Multiplayer", "Singleplayer"],
+        Discount: 30,
+        BeforePrice: 69.99,
+        AfterPrice: 48.99,
+    },
+
+    {
+        ImgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2246340/ac2357a6167bf543b72c7a69ba7fa647013e1f62/hero_capsule.jpg?t=1766102696",
+        BgUrl: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2246340/ss_d439e321350c004c4d866c248019aefdc6a50600.600x338.jpg",
+        VideoUrl: "https://video.fastly.steamstatic.com/store_trailers/2246340/1175711887/4753edf8bca76e24d035ed8919a6d8bd52fbc13d/1765521776/microtrailer.webm?t=1765526396",
+        Title: "Monster Hunter Wilds",
+        ReviewSumary: "Mixed",
+        ReviewStat: 76522,
+        Genres: ["Hunting", "Action", "Multiplayer", "Online Co-Op", "Co-op"],
+        Discount: 45,
+        BeforePrice: 69.99,
+        AfterPrice: 38.49,
+    }
+]
+export const deepGames: DeepGameInfo[] = [
+    {                  
+        Name:"PAYDAY2",
+        Discount: 90,
+        BeforePrice: 9.99,
+        AfterPrice: 0.99,
+        VideoUrl:"https://video.fastly.steamstatic.com/store_trailers/218620/834105061/2d7c8f7b8a0a705cc5bbf4ccc720f29b61e876d9/1750481367/microtrailer.webm?t=1747224986", 
+        Img:"src/assets/capsule_616x353.jpg"
+    },
+    {   
+        Name:"ICARUS",
+        Discount: 90,
+        BeforePrice: 34.99,
+        AfterPrice: 3.49,
+        VideoUrl:"https://video.fastly.steamstatic.com/store_trailers/1149460/351075938/a7a0c966ff56875676dcce09e3f810643d452a98/1753055228/microtrailer.webm?t=1753056808" ,
+        Img:'src/assets/capsule_616x353(1).jpg'
+    },
+    {   
+        Name:"DEADISLAND2",
+        Discount: 90,
+        BeforePrice: 49.99,
+        AfterPrice: 4.99,
+        VideoUrl:"https://video.akamai.steamstatic.com/store_trailers/934700/772874/2b9a66eedd5f57b6934b700b41404ac40ccc8e12/1750597891/microtrailer.webm?t=1729610163",
+        Img:'src/assets/capsule_616x353(2).jpg'
+    },
+    {   
+        Name:"DARKSIDERS3",
+        Discount: 90,
+        BeforePrice: 39.99,
+        AfterPrice: 3.99,
+        VideoUrl:"https://video.fastly.steamstatic.com/store_trailers/606280/100841/ac926393614dbd644670fa581afb8fa25330de99/1750561300/microtrailer.webm?t=1498220875",
+        Img:'src/assets/capsule_616x353(3).jpg'
+    },
+    {   
+        Name:"NIOH",
+        Discount: 90,
+        BeforePrice: 49.99,
+        AfterPrice: 4.99,
+        VideoUrl:"https://video.fastly.steamstatic.com/store_trailers/485510/131812/89af21ae4188b96860383d8f6061a07574a68d6b/1750536695/microtrailer.webm?t=1509956277",
+        Img:'src/assets/capsule_616x353(4).jpg'
+    },
+    {   
+        Name:"LEGOSTARWARS",
+        Discount: 90,
+        BeforePrice: 49.99,
+        AfterPrice: 4.99,
+        VideoUrl:"https://video.fastly.steamstatic.com/store_trailers/920210/510576/f1a07979227ba5c5a4592888deadc7aba83953f1/1750596268/microtrailer.webm?t=1667432123",
+        Img:'src/assets/capsule_616x353(5).jpg'
+    },
+    {   
+        Name:"ASSASSINSCREEDVALHALLA",
+        Discount: 90,
+        BeforePrice: 59.99,
+        AfterPrice: 5.99,
+        VideoUrl:"https://video.fastly.steamstatic.com/store_trailers/2208920/515678/08b6d5b1267d155673daea051094ee1046ddfff7/1750809553/microtrailer.webm?t=1693577111",
+        Img:'src/assets/capsule_616x353(6).jpg'
+    },
+    {   
+        Name:"BENDY",
+        Discount: 90,
+        BeforePrice: 19.99,
+        AfterPrice: 1.99,
+        VideoUrl:"https://video.akamai.steamstatic.com/store_trailers/622650/567000514/b1ac45121f429341b8f0f0f9accd138570ec6d83/1750563536/microtrailer.webm?t=1746814531",
+        Img:'src/assets/capsule_616x353(7).jpg'
+    },
+    {   
+        Name:"FALLOUT76",
+        Discount: 90,
+        BeforePrice: 39.99,
+        AfterPrice: 3.99,
+        VideoUrl:"https://video.fastly.steamstatic.com/store_trailers/1151340/664076456/5e6404afc7c8d33767cbca13f27299652030d711/1756316590/microtrailer.webm?t=1756832424",
+        Img:'src/assets/capsule_616x353(8).jpg'
+    }
+]
+export const fGames: DeepGameInfo[]=[
+    {
+        Name:"Devil May Cry5",
+        Discount: 75,
+        BeforePrice: 29.99,
+        AfterPrice: 7.49, 
+        Img:"src/assets/capsule_616x353(18).jpg"
+    },
+    {
+        Name:"Metaphor: ReFantazio",
+        Discount: 50,
+        BeforePrice: 69.99,
+        AfterPrice: 34.99, 
+        Img:"src/assets/capsule_616x353(19).jpg"
+    },
+    {
+        Name:"NieR:Automata™",
+        Discount: 60,
+        BeforePrice: 39.99,
+        AfterPrice: 15.49, 
+        Img:"src/assets/capsule_616x353(20).jpg"
+    },
+    {
+        Name:"VEIN",
+        Discount: 15,
+        BeforePrice: 19.99,
+        AfterPrice: 16.99, 
+        Img:"src/assets/capsule_616x353(21).jpg"
+    },
+    {
+        Name:"Tom Clancy's Ghost Recon® Breakpoint",
+        Discount: 90,
+        BeforePrice: 59.99,
+        AfterPrice: 5.99, 
+        Img:"src/assets/capsule_616x353(22).jpg"
+    },
+    {
+        Name:"GUILTY GEAR STRIVE",
+        Discount: 50,
+        BeforePrice: 39.99,
+        AfterPrice: 19.99, 
+        Img:"src/assets/capsule_616x353(23).jpg"
+    },
+    {
+        Name:"SnowRunner",
+        Discount: 60,
+        BeforePrice: 29.99,
+        AfterPrice: 11.99, 
+        Img:"src/assets/capsule_616x353(24).jpg"
+    },
+    {
+        Name:"Wildgate",
+        Discount: 50,
+        BeforePrice: 29.99,
+        AfterPrice: 14.99, 
+        Img:"src/assets/capsule_616x353(25).jpg"
+    },
+    {
+        Name:"Broken Arrow",
+        Discount: 15,
+        BeforePrice: 49.99,
+        AfterPrice: 42.49, 
+        Img:"src/assets/capsule_616x353(26).jpg"
+    },
+    {
+        Name:"Overcooked! 2",
+        Discount: 75,
+        BeforePrice: 24.99,
+        AfterPrice: 6.29, 
+        Img:"src/assets/capsule_616x353(27).jpg"
+    },
+    {
+        Name:"Planet Coaster 2",
+        Discount: 50,
+        BeforePrice: 49.99,
+        AfterPrice: 24.99, 
+        Img:"src/assets/capsule_616x353(28).jpg"
+    },
+    {
+        Name:"Witchfire",
+        Discount: 20,
+        BeforePrice: 39.99,
+        AfterPrice: 31.99, 
+        Img:"src/assets/capsule_616x353(29).jpg"
+    },
+    {
+        Name:"Oxygen Not Included",
+        Discount: 70,
+        BeforePrice: 24.99,
+        AfterPrice: 7.49, 
+        Img:"src/assets/capsule_616x353(30).jpg"
+    },
+    {
+        Name:"FINAL FANTASY VII REBIRTH",
+        Discount: 50,
+        BeforePrice: 69.99,
+        AfterPrice: 34.99, 
+        Img:"src/assets/capsule_616x353(31).jpg"
+    },
+    {
+        Name:"Sea of Thieves: 2025 Edition",
+        Discount: 65,
+        BeforePrice: 39.99,
+        AfterPrice: 13.99, 
+        Img:"src/assets/capsule_616x353(32).jpg"
+    },
+    {
+        Name:"Valheim",
+        Discount: 50,
+        BeforePrice: 19.99,
+        AfterPrice: 9.99, 
+        Img:"src/assets/capsule_616x353(33).jpg"
+    }
+    
+]

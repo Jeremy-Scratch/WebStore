6e0b9f9aed4ce42877f2b134fe9159293714d361
Scratch <Jeremyscratch6@gmail.com>
2026-01-18 16:57:43 -0400

    ADD- component html/css structure

--- src/components/carousel/categories/categories.tsx
+++ src/components/carousel/categories/categories.tsx
@@ -1,9 +1,25 @@
 
 export default function () {
-    return(
+    return (
         <div className="max-w-325 w-auto box-border px-12.5 py-6.25 ml-auto mr-auto">
+            <div className="relative font-['Motiva_Sans',Sans-serif] font-medium ">
+                <div id="title" className="flex text-white pr-2.5 pb-2.5 flex-wrap text-center gap-5 font-[5px] ">Browse by Category</div>
+                <div id="hubCarousel" >
+                    <div id="conteiner" className="relative">
+                        <div id="track" className="relative">{/* Mapping */}
+                            <div className=" w-full flex gap-3 transition-opacity duration-400 "> {/* key */}
+                                {/* Mapping */}
+                    {/* card */}<a className="w-[19%] relative max-w-75 rounded-[10px] overflow-hidden h-55.25 shrink-0 decoration-0 " href=""></a> {/* key */}
+                                {/* 4 cards */}
+                            </div>
+                        </div>
+                        {/* arrow */}
+                        {/* arrow */}
+                        {/* thumbs */}
+                    </div>
 
+                </div>
+            </div>
         </div>
-
     );
 }

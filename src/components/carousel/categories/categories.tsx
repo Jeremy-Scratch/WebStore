6b09ab73fc834628b8ff84d4f8b28a2d44985874
Scratch <Jeremyscratch6@gmail.com>
2026-01-19 00:26:25 -0400

    ADD- CSS effects - aria-hidden

--- src/components/carousel/categories/categories.tsx
+++ src/components/carousel/categories/categories.tsx
@@ -1,9 +1,9 @@
 import CategoryCard from "./categoryCard";
-import { CategoriesData, CategoryGradient} from "../../../data/gamesObjects";
+import { CategoriesData, CategoryGradient } from "../../../data/gamesObjects";
 import useCarousel from "../../../hooks/carouselLogic";
 
 export default function CategoryCarousel() {
-    const { gamePacks } = useCarousel(CategoriesData, 5);
+    const { gamePacks, currentIndex } = useCarousel(CategoriesData, 5);
 
     return (
         <div className="max-w-325 w-auto box-border px-12.5 py-6.25 ml-auto mr-auto">
@@ -11,25 +11,26 @@ export default function CategoryCarousel() {
                 <div id="title" className="flex text-white pr-2.5 pb-2.5 flex-wrap text-center gap-2 text-[20px] font-semibold ">Browse by Category</div>
                 <div id="hubCarousel" >
                     <div id="conteiner" className="relative">
-                        <div id="track" className="relative">{gamePacks.map((category, index) => {
+                        <div id="track" className="relative">{gamePacks.map((categories, index) => {
+                            const isActive = index === currentIndex;
                             return (
-                                <div key={index} className=" w-full flex gap-3 transition-opacity duration-400 ">{category.map((cate,index) => {
-                                     cate.CN = CategoryGradient[index];
-                                    return (
-                                        <CategoryCard key={index} categoryInfo={cate}/>
-                                );
-                                })}
-
+                                <div key={index}
+                                    aria-hidden={!isActive}
+                                    className={`w-full flex gap-3 opacity-0 absolute transition-opacity duration-400 ${isActive ? "opacity-100 relative" : ""}`}>
+                                        {categories.map((category, index) => {
+                                        category.CN = CategoryGradient[index];
+                                        return (
+                                            <CategoryCard key={index} categoryInfo={category} />
+                                        );
+                                    })}
                                 </div>
                             );
                         })}
-
                         </div>
                         {/* arrow */}
                         {/* arrow */}
                         {/* thumbs */}
                     </div>
-
                 </div>
             </div>
         </div>

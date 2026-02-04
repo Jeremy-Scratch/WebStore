b55c7da97e3404cfaab9ba3ad6e910aa91390a6c
Scratch <Jeremyscratch6@gmail.com>
2026-02-04 15:23:27 -0400

    MODIFY: variables name

--- src/components/banners.tsx
+++ src/components/banners.tsx
@@ -5,7 +5,7 @@ export default function Banners() {
     return (
         <div className="flex gap-12.5 max-w-325 ml-auto mr-auto w-screen py-6.25 px-12.5 box-border overflow-hidden ">
             <a className="transition-transform duration-150 ease-in-out hover:-translate-y-0.75 " href=""><img src={giftBanner} alt="Gift card banner" /></a>
-            <a className="transition-transform duration-150 ease-in-out hover:-translate-y-0.75 " href=""><img src={awardBanner} alt="Steam Awards" /></a>
+            <a className="transition-transform duration-150 ease-in-out hover:-translate-y-0.75 " href=""><img src={awardBanner} alt="Awards" /></a>
         </div>
     )
 }

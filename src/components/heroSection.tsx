c7e49dd356561cd19c72d3b2761f517e5d24cf7c
Scratch <Jeremyscratch6@gmail.com>
2026-02-05 20:57:53 -0400

    Chance: Img URL

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -1,13 +1,11 @@
+import heroImg from '../assets/heroblock.png'
 
 export default function heroSection() {
     return (
         <div className=" z-1 overflow-hidden w-full box-content">
-            <video  autoPlay loop muted playsInline preload=""
-                    className="-translate-x-2/4 ml-[50%] ">
-                <source src="https://shared.fastly.steamstatic.com/store_item_assets/steam/clusters/seasonalsales/510eefba7ced02981bca521c/webm_page_bg_english.webm?t=1765401325" 
-                        type="video/webm"
-                        className=" max-h-150 " />
-            </video>
+
+            <img src={heroImg} alt="" className="-translate-x-2/4 ml-[50%]  max-h-150 " />
+
         </div>
     );
 }

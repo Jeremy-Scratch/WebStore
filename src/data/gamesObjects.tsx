e32366ba27d9cba6629ec9987adbd60f5392dd80
Scratch <Jeremyscratch6@gmail.com>
2026-02-10 23:50:35 -0400

    NEW: new assets for Games

--- src/data/gamesObjects.tsx
+++ src/data/gamesObjects.tsx
@@ -195,7 +195,7 @@ export const deepGames: DeepGameInfo[] = [
         Img:'src/assets/capsule_616x353(8).jpg'
     }
 ]
-export const fGames: DeepGameInfo[]=[
+export const fGames: DeepGameInfo[] = [
     {
         Name:"Devil May Cry5",
         Discount: 75,

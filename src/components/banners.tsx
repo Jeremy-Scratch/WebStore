ba3bad1fa98650a18d48dfabb750b3eff3c0ae15
Scratch <Jeremyscratch6@gmail.com>
2026-02-13 18:11:21 -0400

    NEW: Assets Banners \ change url

--- src/components/banners.tsx
+++ src/components/banners.tsx
@@ -1,11 +1,10 @@
-import giftBanner from '../assets/gift_card_banner_desktop_english.webp'
-import awardBanner from '../assets/steam_award_banner_desktop_english.webp'
+import giftBanner from '../assets/banner_giftcard.png'
 
 export default function Banners() {
     return (
         <div className="flex gap-12.5 max-w-325 ml-auto mr-auto w-screen py-6.25 px-12.5 box-border overflow-hidden ">
-            <a className="transition-transform duration-150 ease-in-out hover:-translate-y-0.75 " href=""><img src={giftBanner} alt="Gift card banner" /></a>
-            <a className="transition-transform duration-150 ease-in-out hover:-translate-y-0.75 " href=""><img src={awardBanner} alt="Awards" /></a>
+            <a className="transition-transform duration-150 ease-in-out hover:-translate-y-0.75 " href=""><img src={giftBanner} alt="Gift card banner" className='w-148' /></a>
+            <a className="transition-transform duration-150 ease-in-out hover:-translate-y-0.75 " href=""><img src={giftBanner} alt="Gift card banner" className='w-148' /></a>
         </div>
     )
 }
